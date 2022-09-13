e4d679fe60dfc0606dc3d87dcb88593279e13f41
Yazcodes <88701749+thisthatt@users.noreply.github.com>
2022-09-13 12:10:20 -0400

    added files

---
+++ server.js
@@ -0,0 +1,29 @@
+﻿const express  = require('express')
+const app = express()
+const cors = require('cors')
+const myDB={
+        'name': 'Luke Skywalker',
+        'height': '172',
+        'mass': '77',
+        'hair_color': 'blond',
+        'skin_color': 'fair',
+        'eye_color': 'blue',
+        'birth_year': '19BBY',
+        'gender': 'male',
+    
+    
+}
+
+app.get('/', (req,res)=>{
+
+res.send('Api server')
+})
+app.get('/api',(req,res)=>{
+    res.json(myDB)
+})
+
+const PORT = 3001
+app.listen(process.env.PORT || PORT ,()=>{
+    console.log(`Server started on PORT:${PORT}`)
+
+})
